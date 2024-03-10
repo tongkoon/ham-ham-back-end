e6dcefdf5d46b712dcde2dbb8a8f876367f1f7da
tongkoon <64011212078@msu.ac.th>
2024-03-10 17:30:44 +0700

    register

--- api/user.ts
+++ api/user.ts
@@ -34,12 +34,15 @@ router.post('/register', upload.single('avatar'), async (req, res) => {
     let sql = 'select * from user where username = ?';
     sql = mysql.format(sql, [user.username])
     conn.query(sql, async (err, result) => {
+        // check username is duplicate
         if (err) {
             res.status(400)
                 .json(err)
         }
         else {
+            // check avatar is null
             if (req.file?.originalname == undefined) {
+                // set image defaul
                 downloadUrl = 'https://firebasestorage.googleapis.com/v0/b/test-upload-image-44e6d.appspot.com/o/files%2FDefaultPic.png?alt=media&token=d87a7086-a6e3-4814-bc01-bdf8722f4c7b'
             } else {
                 const storageRef = ref(storage, `files/${req.file?.originalname}`);
